33e379feb0d465c4c151f36d377e4ca261e22d57
nikhilc52 <nsc49@cornell.edu>
2025-03-29 18:41:10 -0400

    added functionality for dots

--- css-js/script.js
+++ css-js/script.js
@@ -1,5 +1,8 @@
 const image = document.getElementById("image");
+image.classList.toggle('blur');
+
 const aboutdesc = document.getElementById("about-desc");
+// initially fade to invert
 aboutdesc.classList.toggle('fade-opacity');
 
 const about = document.getElementById("about");
@@ -9,6 +12,7 @@ const more = document.getElementById("more");
 more.addEventListener('click', toggleBlurMore);
 
 const projects = document.getElementById("projects");
+// initially fade(s) to invert(s)
 projects.classList.toggle('fade-opacity');
 
 const personal = document.getElementById("personal");
@@ -21,7 +25,6 @@ function toggleBlurAbout() {
     image.classList.toggle('blur');
     aboutdesc.classList.toggle('fade-opacity');
     more.classList.toggle('half-fade');
-    console.log(image.offsetWidth / 2)
 }
 
 function toggleBlurMore() {
@@ -29,8 +32,44 @@ function toggleBlurMore() {
     about.classList.toggle('half-fade');
     projects.classList.toggle('fade-opacity');
     personal.classList.toggle('fade-opacity');
-    everything.classList.toggle('fade-opacity'); const magazine = document.getElementById("magazine");
-    magazine.style.left = (image.offsetWidth / 2) + "px"
+    everything.classList.toggle('fade-opacity');
 }
 
+function resizeEverything() {
+    resizeImage()
+    resizeIcons()
+}
 
+function resizeImage() {
+    if (1.333 <= (window.innerWidth / window.innerHeight) && (window.innerWidth / window.innerHeight) <= 1.777) {
+        image.style.height = window.innerHeight + "px"
+        image.style.width = window.innerHeight * (1920 / 1080) + "px"
+    }
+    // width >> height : too wide
+    else if (1.777 <= window.innerWidth / window.innerHeight) {
+        image.style.width = window.innerWidth + "px"
+        image.style.height = window.innerWidth * (1080 / 1920) + "px"
+    }
+    // width << height : too tall
+    else {
+        image.style.height = window.innerHeight + "px"
+        image.style.width = window.innerHeight * (1920 / 1080) + "px"
+    }
+    window.scrollTo({
+        top: (document.documentElement.scrollHeight - window.innerHeight) / 2,
+        left: (document.documentElement.scrollWidth - window.innerWidth) / 2,
+        behavior: "smooth"
+    });
+}
+
+function resizeIcons() {
+    dots = document.getElementsByClassName("pointer");
+    magazine = dots[0];
+    magazine.style.top = .45 * (document.documentElement.scrollHeight) + "px"
+    magazine.style.left = .39 * (document.documentElement.scrollWidth) + "px"
+}
+
+window.onresize = resizeEverything
+
+resizeEverything()
+image.classList.toggle('blur');
