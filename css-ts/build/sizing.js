a356d0153c9341b21652cdfb0951913361d899fd
nikhilc52 <nsc49@cornell.edu>
2026-06-06 03:06:14 -0400

    Updated dots for networks article/book
    - Also moved Win to projects
    - And changed Google Analytics tag to inside <Head>

--- css-ts/build/sizing.js
+++ css-ts/build/sizing.js
@@ -50,34 +50,38 @@ function animateIcon(elem) {
 }
 // place icons at given %'s
 function rePlaceIcons() {
-    rePlaceIcon(document.getElementById("basketball"), document.getElementById("basketball-circle"), .374, .449);
-    rePlaceText(document.getElementById("basketball-desc"), .394, .445);
-    rePlaceIcon(document.getElementById("flag"), document.getElementById("flag-circle"), .35, .487);
-    rePlaceText(document.getElementById("flag-desc"), .37, .487);
-    rePlaceIcon(document.getElementById("crossword"), document.getElementById("crossword-circle"), .363, .545);
-    rePlaceText(document.getElementById("crossword-desc"), .383, .51);
-    rePlaceIcon(document.getElementById("building"), document.getElementById("building-circle"), .31, .587);
-    rePlaceText(document.getElementById("building-desc"), .33, .587);
+    rePlaceIcon(document.getElementById("basketball"), document.getElementById("basketball-circle"), .367, .447);
+    rePlaceText(document.getElementById("basketball-desc"), .387, .447);
+    rePlaceIcon(document.getElementById("flag"), document.getElementById("flag-circle"), .35, .493);
+    rePlaceText(document.getElementById("flag-desc"), .37, .493);
+    rePlaceIcon(document.getElementById("crossword"), document.getElementById("crossword-circle"), .366, .535);
+    rePlaceText(document.getElementById("crossword-desc"), .386, .535);
+    rePlaceIcon(document.getElementById("building"), document.getElementById("building-circle"), .314, .576);
+    rePlaceText(document.getElementById("building-desc"), .334, .576);
     rePlaceIcon(document.getElementById("resume"), document.getElementById("resume-circle"), .617, .54);
     rePlaceText(document.getElementById("resume-desc"), .637, .54);
+    rePlaceIcon(document.getElementById("cdj"), document.getElementById("cdj-circle"), .675, .523);
+    rePlaceText(document.getElementById("cdj-desc"), .695, .523);
     rePlaceIcon(document.getElementById("computer"), document.getElementById("computer-circle"), .55, .60);
     rePlaceText(document.getElementById("computer-desc"), .57, .6);
+    rePlaceIcon(document.getElementById("book"), document.getElementById("book-circle"), .32, .605);
+    rePlaceText(document.getElementById("book-desc"), .34, .605);
     rePlaceIcon(document.getElementById("calendar"), document.getElementById("calendar-circle"), .28, .465);
     rePlaceText(document.getElementById("calendar-desc"), .30, .465);
     rePlaceIcon(document.getElementById("logo"), document.getElementById("logo-circle"), .7, .51);
     rePlaceText(document.getElementById("logo-desc"), .72, .51);
     rePlaceIcon(document.getElementById("fortune"), document.getElementById("fortune-circle"), .315, .496);
-    rePlaceText(document.getElementById("fortune-desc"), .325, .496);
+    rePlaceText(document.getElementById("fortune-desc"), .335, .496);
     rePlaceIcon(document.getElementById("drawing"), document.getElementById("drawing-circle"), .243, .583);
-    rePlaceText(document.getElementById("drawing-desc"), .273, .583);
+    rePlaceText(document.getElementById("drawing-desc"), .263, .583);
     rePlaceIcon(document.getElementById("magazine"), document.getElementById("magazine-circle"), .42, .39);
     rePlaceText(document.getElementById("magazine-desc"), .44, .39);
     rePlaceIcon(document.getElementById("vinyl"), document.getElementById("vinyl-circle"), .215, .48);
     rePlaceText(document.getElementById("vinyl-desc"), .235, .48);
     rePlaceIcon(document.getElementById("canvas"), document.getElementById("canvas-circle"), .27, .499);
     rePlaceText(document.getElementById("canvas-desc"), .29, .499);
-    rePlaceIcon(document.getElementById("gunner-book"), document.getElementById("gunner-book-circle"), .28, .622);
-    rePlaceText(document.getElementById("gunner-book-desc"), .30, .622);
+    rePlaceIcon(document.getElementById("gunner-book"), document.getElementById("gunner-book-circle"), .23, .629);
+    rePlaceText(document.getElementById("gunner-book-desc"), .25, .629);
     rePlaceIcon(document.getElementById("coates-book"), document.getElementById("coates-book-circle"), .82, .53);
     rePlaceText(document.getElementById("coates-book-desc"), .84, .53);
 }
